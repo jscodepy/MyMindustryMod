c88344a5cc1526fe4e84afa0d5f5af4c898c9a4c
WswDay2022 <mcmmmpoj@163.com>
2024-08-20 21:35:20 +0800

    T_T·····

--- src/morefactories/MoreFactories.java
+++ src/morefactories/MoreFactories.java
@@ -1,7 +1,11 @@
 package morefactories;
 
+import arc.Core;
+import arc.Events;
 import arc.util.*;
+import mindustry.game.EventType;
 import mindustry.mod.*;
+import mindustry.ui.dialogs.BaseDialog;
 import morefactories.blocks.DieselOilGenericCrafter;
 import morefactories.blocks.DieselSynthesisEngine;
 import morefactories.liquids.DieselOil;
@@ -9,13 +13,21 @@ import morefactories.liquids.DieselOil;
 public class MoreFactories extends Mod{
 
     public MoreFactories() {
-        // No more write to here.
-        // Maybe? I don't know
+        Log.info("Loaded ExampleJavaMod constructor.");
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("frog");
+                dialog.cont.add("behold").row();
+                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
     }
     @Override
     public void loadContent(){
-        // Hey,I took one hour just found this way to write loads!
-        // QWQ
         DieselOil.load();
         DieselOilGenericCrafter.load();
         DieselSynthesisEngine.load();
