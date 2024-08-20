5170d0471102b0d207f902e6d82fe052506c6773
WswDay2022 <mcmmmpoj@163.com>
2024-08-20 18:04:25 +0800

    awa

--- src/morefactories/MoreFactories.java
+++ src/morefactories/MoreFactories.java
@@ -1,27 +1,23 @@
-package example;
+package morefactories;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class MoreFactories extends Mod{
 
-    public ExampleJavaMod(){
+    public MoreFactories(){
         Log.info("Loaded ExampleJavaMod constructor.");
-
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                //mod sprites are prefixed with the mod name (this mod is called 'morefactories-java-mod' in its config)
+                dialog.cont.image(Core.atlas.find("morefactories-java-mod-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
@@ -30,7 +26,7 @@ public class ExampleJavaMod extends Mod{
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("Loading some morefactories content.");
     }
 
 }
