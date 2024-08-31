9c44ce4197a4ec3b57bbc42168566f30cb99f2d1
WswDay2022 <mcmmmpoj@163.com>
2024-08-31 23:06:20 +0800

    version 0.1 alpha RELAY

--- src/morefactories/content/MoreFactoriesBlocks.java
+++ src/morefactories/content/MoreFactoriesBlocks.java
@@ -5,6 +5,7 @@ import mindustry.content.Fx;
 import mindustry.content.Items;
 import mindustry.content.Liquids;
 import mindustry.content.StatusEffects;
+import mindustry.entities.UnitSorts;
 import mindustry.entities.bullet.BasicBulletType;
 import mindustry.entities.bullet.ContinuousLaserBulletType;
 import mindustry.entities.bullet.LaserBulletType;
@@ -12,6 +13,7 @@ import mindustry.entities.effect.MultiEffect;
 import mindustry.entities.pattern.ShootAlternate;
 import mindustry.gen.Sounds;
 import mindustry.graphics.Pal;
+import mindustry.logic.LExecutor;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.type.LiquidStack;
@@ -43,7 +45,10 @@ public class MoreFactoriesBlocks {
     public static Block plastaniumConveyor;
     public static Block flames;
     public static Block disappearing;
-    public MoreFactoriesBlocks() {}
+
+    public MoreFactoriesBlocks() {
+    }
+
     public static void load() {
         largeKiln = new GenericCrafter("large-kiln") {{ // 大型窑炉
             this.hasPower = true; // 是否需要用电
@@ -58,9 +63,9 @@ public class MoreFactoriesBlocks {
             // this.drawer = new DrawMulti(new DrawDefault(), new DrawFlame(Color.valueOf("ffc099")));
             this.outputItem = new ItemStack(Items.metaglass, 3); // 输出物品
 
-            this.consumeItems(ItemStack.with(Items.lead,3,Items.sand,3)); // 消耗物品
-            this.consumeLiquid(Liquids.water,5f / 60f).boost(); // 消耗液体
-            this.consumeLiquid(Liquids.cryofluid,5f / 60f).boost();
+            this.consumeItems(ItemStack.with(Items.lead, 3, Items.sand, 3)); // 消耗物品
+            this.consumeLiquid(Liquids.water, 5f / 60f).boost(); // 消耗液体
+            this.consumeLiquid(Liquids.cryofluid, 5f / 60f).boost();
             this.consumePower(2f); // 消耗电力
             this.requirements(Category.crafting, ItemStack.with(Items.copper, 130, Items.metaglass, 80, Items.plastanium, 35, Items.silicon, 60, Items.lead, 50));
             // 选项卡和建造所需材料/\/\
@@ -76,8 +81,8 @@ public class MoreFactoriesBlocks {
             this.liquidCapacity = 10f;
             // this.drawer = new DrawMulti(new DrawDefault(), new DrawWarmupRegion(), new DrawLiquidRegion());
 
-            this.consumeLiquid(Liquids.water,5f / 60f);
-            this.requirements(Category.power,ItemStack.with(Items.copper,15,Items.lead,15,Items.silicon,18,Items.metaglass,15));
+            this.consumeLiquid(Liquids.water, 5f / 60f);
+            this.requirements(Category.power, ItemStack.with(Items.copper, 15, Items.lead, 15, Items.silicon, 18, Items.metaglass, 15));
         }};
         dieselOilAlternator = new ConsumeGenerator("diesel-oil-alternator") {{ // 柴油发电机
             this.powerProduction = 28f;
@@ -90,8 +95,8 @@ public class MoreFactoriesBlocks {
             this.liquidCapacity = 50f;
             this.drawer = new DrawMulti(new DrawRegion("-bottom"), new DrawLiquidTile(MoreFactoriesLiquids.dieselOil), new DrawDefault());
 
-            this.consumeLiquid(MoreFactoriesLiquids.dieselOil,25f / 60f);
-            this.requirements(Category.power,ItemStack.with(Items.copper,20,Items.lead,15,Items.silicon,18,Items.metaglass,13));
+            this.consumeLiquid(MoreFactoriesLiquids.dieselOil, 25f / 60f);
+            this.requirements(Category.power, ItemStack.with(Items.copper, 20, Items.lead, 15, Items.silicon, 18, Items.metaglass, 13));
         }};
         dieselOilCrafter = new GenericCrafter("diesel-oil-crafter") {{ // 柴油混合机
             this.size = 2;
@@ -102,15 +107,17 @@ public class MoreFactoriesBlocks {
             this.liquidCapacity = 20f;
             this.craftTime = 130;
             this.envEnabled = Env.any;
-            this.drawer = new DrawMulti(new DrawRegion("-bottom"), new DrawLiquidTile(Liquids.oil), new DrawLiquidTile(MoreFactoriesLiquids.dieselOil){{drawLiquidLight = true;}}, new DrawDefault());
-            this.outputLiquid = new LiquidStack(MoreFactoriesLiquids.dieselOil,12f / 60f);
+            this.drawer = new DrawMulti(new DrawRegion("-bottom"), new DrawLiquidTile(Liquids.oil), new DrawLiquidTile(MoreFactoriesLiquids.dieselOil) {{
+                drawLiquidLight = true;
+            }}, new DrawDefault());
+            this.outputLiquid = new LiquidStack(MoreFactoriesLiquids.dieselOil, 12f / 60f);
             this.hasLiquids = true;
             this.hasPower = true;
             this.hasItems = true;
             this.lightLiquid = MoreFactoriesLiquids.dieselOil;
 
-            this.consumeItem(Items.sporePod,2);
-            this.consumeLiquid(Liquids.oil,12f / 60f);
+            this.consumeItem(Items.sporePod, 2);
+            this.consumeLiquid(Liquids.oil, 12f / 60f);
             this.consumePower(1.2f);
             this.requirements(Category.crafting, ItemStack.with(Items.copper, 30, Items.lead, 30, Items.plastanium, 20, Items.silicon, 25));
         }};
@@ -124,13 +131,13 @@ public class MoreFactoriesBlocks {
             this.envEnabled |= Env.space;
 
             this.consumePower(0.91f);
-            this.requirements(Category.distribution, ItemStack.with(Items.plastanium, 6, Items.silicon, 5, Items.lead, 8, Items.graphite, 5,MoreFactoriesItems.titaniumAlloy,8));
+            this.requirements(Category.distribution, ItemStack.with(Items.plastanium, 6, Items.silicon, 5, Items.lead, 8, Items.graphite, 5, MoreFactoriesItems.titaniumAlloy, 8));
         }};
         // 下面是炮塔
         flames = new ItemTurret("flames") {{ // 火舌
-            this.requirements(Category.turret, with(Items.copper, 1200, Items.graphite, 1300, Items.surgeAlloy, 800, Items.plastanium, 580, Items.thorium, 1000,MoreFactoriesItems.gold,320,MoreFactoriesItems.iron,700));
+            this.requirements(Category.turret, with(Items.copper, 1200, Items.graphite, 1300, Items.surgeAlloy, 800, Items.plastanium, 580, Items.thorium, 1000, MoreFactoriesItems.gold, 320, MoreFactoriesItems.iron, 700));
             this.ammo(
-                    Items.graphite, new BasicBulletType(8f, 100){{
+                    Items.graphite, new BasicBulletType(8f, 100) {{
                         hitSize = 5.5f;
                         width = 16f;
                         height = 21f;
@@ -139,7 +146,7 @@ public class MoreFactoriesBlocks {
                         reloadMultiplier = 1.7f;
                         knockback = 0.65f;
                     }},
-                    Items.thorium, new BasicBulletType(9f, 120){{
+                    Items.thorium, new BasicBulletType(9f, 120) {{
                         hitSize = 6;
                         width = 17f;
                         height = 22f;
@@ -148,7 +155,7 @@ public class MoreFactoriesBlocks {
                         pierceBuilding = true;
                         knockback = 0.85f;
                     }},
-                    Items.pyratite, new BasicBulletType(9f, 110){{
+                    Items.pyratite, new BasicBulletType(9f, 110) {{
                         hitSize = 5.2f;
                         width = 17f;
                         height = 22f;
@@ -165,7 +172,7 @@ public class MoreFactoriesBlocks {
                         splashDamage = 20f;
                         splashDamageRadius = 25f;
                     }},
-                    MoreFactoriesItems.titaniumAlloy,new BasicBulletType(9f,135) {{
+                    MoreFactoriesItems.titaniumAlloy, new BasicBulletType(9f, 135) {{
                         hitSize = 6;
                         width = 17f;
                         height = 22f;
@@ -194,44 +201,48 @@ public class MoreFactoriesBlocks {
             this.limitRange();
         }};
         disappearing = new PowerTurret("disappearing") {{ // 消逝
-            this.requirements(Category.turret, with(Items.copper, 2500, Items.lead, 1900, Items.graphite, 2800, Items.surgeAlloy, 2400, Items.silicon, 1950,MoreFactoriesItems.gold,1000,MoreFactoriesItems.titaniumAlloy ,500));
-            this.shootEffect = Fx.shootBigSmoke2;
-            this.shootCone = 40f;
-            this.recoil = 6f;
-            this.recoilTime = 3.5f;
-            this.size = 4;
-            this.shake = 2.5f;
+            this.requirements(Category.turret, with(Items.copper, 2500, Items.lead, 1900, Items.graphite, 2800, Items.surgeAlloy, 2400, Items.silicon, 1950, MoreFactoriesItems.gold, 1000, MoreFactoriesItems.titaniumAlloy, 500));
             this.range = 320f;
-            this.reload = 90f;
             this.health = 4700;
-            this.shootSound = Sounds.laserbig;
-            this.loopSound = Sounds.beam;
-            this.loopSoundVolume = 2f;
-            this.envEnabled |= Env.space;
-
-            this.ammoPerShot = 5;
             this.targetAir = this.targetGround = true;
-            this.shootType = new ContinuousLaserBulletType(2000){{
-                this.chargeEffect = new MultiEffect(Fx.teleport);
+            this.shootType = new ContinuousLaserBulletType(2000) {{
                 this.length = 330f;
+                this.chargeEffect = Fx.teleport;
                 this.hitEffect = Fx.dynamicSpikes;
                 this.hitColor = Pal.meltdownHit;
-                this.status = StatusEffects.melting;
+                this.shootEffect = Fx.instShoot;
+                this.despawnEffect = Fx.dynamicSpikes;
+                this.smokeEffect = Fx.smokeCloud;
+                this.trailEffect = Fx.railTrail;
+                this.buildingDamageMultiplier = 0.3f;
                 this.drawSize = 65f;
+                this.speed = range + 20f;
                 this.incendChance = 1f;
                 this.incendSpread = 5.1f;
                 this.incendAmount = 1;
                 this.ammoMultiplier = 1f;
             }};
-            this.scaledHealth = 250;
-            this.coolant = consumeCoolant(0.6f);
-            this.coolant.optional = false;
-            this.itemCapacity = 20;
-            this.minWarmup = 5.2f;
+            this.maxAmmo = 40;
+            this.ammoPerShot = 5;
+            this.rotateSpeed = 1.8f;
+            reload = 230f;
+            ammoUseEffect = Fx.casing3Double;
+            recoil = 7f;
+            recoilTime = recoil * 2f;
+            cooldownTime = reload;
+            shake = 6.2f;
+            size = 4;
+            shootCone = 2f;
+            shootSound = Sounds.railgun;
+            unitSort = UnitSorts.strongest;
+            envEnabled |= Env.space;
+            this.shootEffect = Fx.shootBigSmoke2;
 
-            this.consumeItem(MoreFactoriesItems.titaniumAlloy,5);
+            coolantMultiplier = 0.4f;
+            scaledHealth = 280;
+            coolant = consumeCoolant(1f);
+
             this.consumePower(28f);
         }};
-
     }
 }
